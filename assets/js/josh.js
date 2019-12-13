7f0e9ca798d93e7bb38348e6b107d0eed288b63e
Han Kim <joshhk72@gmail.com>
2019-12-13 16:32:34 -0500

    Fix some padding dude

--- assets/js/josh.js
+++ assets/js/josh.js
@@ -2,12 +2,12 @@
   var text = ["Happy Birthday", "Bryce", ":^)"];
   var counter = 0;
   var elem = document.getElementById("spinner-josh");
-  var inst = setInterval(change, 3000);
+  var inst = setInterval(change, 4000);
 
   function change() {
-    elem.innerHTML = text[counter];
+    elem.innerHTML = text[counter] || text[counter-1];
     counter++;
-    if (counter >= text.length) {
+    if (counter >= text.length + 1) {
       counter = 0;
     }
   }
